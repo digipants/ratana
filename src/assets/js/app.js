9f9903eda9dd3c005550fae32cf7ee758ce86f86
Bhupendra Kumar <bhupendra@digipants.com>
2023-01-25 17:29:40 +0530

    book page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -15254,3 +15254,19 @@ btn.onclick = function () {
 close.onclick = function () {
   popup.style.display = "none";
 }
+
+$(document).ready(function(){
+  var dateToday = new Date();
+  var dates = $("#arrival, #departure").datepicker({
+    defaultDate: "+1w",
+    changeMonth: true,
+    numberOfMonths: 1,
+    minDate: dateToday,
+    onSelect: function (selectedDate) {
+      var option = this.id == "arrival" ? "minDate" : "maxDate",
+        instance = $(this).data("datepicker"),
+        date = $.datepicker.parseDate(instance.settings.dateFormat || $.datepicker._defaults.dateFormat, selectedDate, instance.settings);
+      dates.not(this).datepicker("option", option, date);
+    }
+  });
+});
