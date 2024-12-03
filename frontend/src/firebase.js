0ebe0d5ff9352203390560902c08a266c1bb87af
minioncodes <aryan@digipants.com>
2024-12-03 15:22:04 +0530

    firebase changes

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDTN7n0AocGqwJ_VnZGcDqiaa0m1K4uHdw",
-  authDomain: "ratnaauth.firebaseapp.com",
+  authDomain: "ratanainternational.com",
   projectId: "ratnaauth",
   storageBucket: "ratnaauth.firebasestorage.app",
   messagingSenderId: "1017201036343",
