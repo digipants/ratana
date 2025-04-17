e43d79d6b2961d1e5ff81359a6c45c1cb2071dda
Amankhare01 <amankhare.aa@gmail.com>
2025-04-17 12:56:11 +0530

    block

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About.jsx";
-import Login from "./pages/Login.jsx";
-import Signup from "./pages/Signup.jsx";
-import Profile from "./pages/Profille.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
-import Banquet from "./htmlRoute/Banquet.jsx";
-import Book from "./htmlRoute/Book.jsx";
-import Contact from "./htmlRoute/Contact.jsx";
-import Couple from "./htmlRoute/Couple.jsx";
-import Room from "./htmlRoute/Room.jsx";
-import Jashn from "./htmlRoute/Jashn.jsx";
-import HotelGallery from "./htmlRoute/HotelGallery.jsx";
-import AboutUs from "./htmlRoute/AboutUs.jsx"
-import Policy from "./htmlRoute/Policy.jsx";
-import PrivacyPolicy from "./htmlRoute/PrivacyPolicy.jsx";
-import TermsConditions from "./htmlRoute/TermsConditions.jsx";
-import Thanks from "./htmlRoute/Thanks.jsx";
-import Home from "./htmlRoute/Home.jsx";
+// import About from "./pages/About.jsx";
+// import Login from "./pages/Login.jsx";
+// import Signup from "./pages/Signup.jsx";
+// import Profile from "./pages/Profille.jsx";
+// import PrivateRoute from "./components/PrivateRoute.jsx";
+// import Banquet from "./htmlRoute/Banquet.jsx";
+// import Book from "./htmlRoute/Book.jsx";
+// import Contact from "./htmlRoute/Contact.jsx";
+// import Couple from "./htmlRoute/Couple.jsx";
+// import Room from "./htmlRoute/Room.jsx";
+// import Jashn from "./htmlRoute/Jashn.jsx";
+// import HotelGallery from "./htmlRoute/HotelGallery.jsx";
+// import AboutUs from "./htmlRoute/AboutUs.jsx"
+// import Policy from "./htmlRoute/Policy.jsx";
+// import PrivacyPolicy from "./htmlRoute/PrivacyPolicy.jsx";
+// import TermsConditions from "./htmlRoute/TermsConditions.jsx";
+// import Thanks from "./htmlRoute/Thanks.jsx";
+// import Home from "./htmlRoute/Home.jsx";
+import Block from "./htmlRoute/Block.jsx";
 
 export default function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Block/>}/>
+        {/* <Route path="/about" element={<About />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route element={<PrivateRoute />}>
@@ -40,7 +41,7 @@ export default function App() {
         <Route path="/privacy-policy" element={<PrivacyPolicy/>}/>
         <Route path="/rooms" element={<Room/>}/>
         <Route path="/terms-conditions" element={<TermsConditions/>}/>
-        <Route path="/thanks" element={<Thanks/>}/>
+        <Route path="/thanks" element={<Thanks/>}/> */}
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/htmlRoute/Block.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Block = () => {
+  return (
+    <div
+    >
+      <img
+        src="https://i.pinimg.com/736x/03/03/59/030359d38002933e110400a39aba5e01.jpg"
+        alt="tata"
+        style={{
+            left:'20px',
+          width: '80%',
+          height: '95vh',
+          objectFit: 'cover',
+        }}
+      />
+    </div>
+  );
+};
+
+export default Block;
