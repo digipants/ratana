963d83eb6c98a0b1305e213dcefc1a77805b3356
Amankhare01 <amankhare.aa@gmail.com>
2025-04-17 13:31:25 +0530

    df

--- frontend/src/htmlRoute/Block.jsx
+++ frontend/src/htmlRoute/Block.jsx
@@ -4,7 +4,7 @@ const Block = () => {
   return (
     <div className="black-bg-wrapper">
       <img
-        src="https://i.pinimg.com/736x/03/03/59/030359d38002933e110400a39aba5e01.jpg"
+        src="../assets/images/rahul.jpg"
         alt="tata"
         className="centered-image"
       />
