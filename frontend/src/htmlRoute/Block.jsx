374e639107a4646fa9ec5b668e83162a19436565
Amankhare01 <amankhare.aa@gmail.com>
2025-04-17 13:09:03 +0530

    refactor: clean up Block component and enhance styling with CSS classes

--- frontend/src/htmlRoute/Block.jsx
+++ frontend/src/htmlRoute/Block.jsx
@@ -1,18 +1,12 @@
-import React from 'react';
+import '../index.css';
 
 const Block = () => {
   return (
-    <div
-    >
+    <div className="black-bg-wrapper">
       <img
         src="https://i.pinimg.com/736x/03/03/59/030359d38002933e110400a39aba5e01.jpg"
         alt="tata"
-        style={{
-            left:'20px',
-          width: '80%',
-          height: '95vh',
-          objectFit: 'cover',
-        }}
+        className="centered-image"
       />
     </div>
   );
